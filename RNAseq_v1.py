223223ff5e69de4b2d4f1837003d894be318c102
zhusitao1990 <zhusitao1990@163.com>
2021-06-21 20:42:02 +0800

    continue

--- RNAseq_v1.py
+++ RNAseq_v1.py
@@ -1,217 +1,252 @@
 #coding:utf-8
 #!/usr/bin/python
 
-import os,sys
-from optparse import OptionParser
+import os
+import re
+import sys
+import json
+import argparse
 
-##python 2.7版本支持
-# for python 3.6 
-parser = OptionParser(usage="%prog [options] Samples_List Configure")
+## python 3.6 supported
+## modefied 20210528
+## Auto generate analysis shell script
 
-parser.add_option("-t", "--timeout",
-                action = "store",
-                type = 'int',
-                dest = "timeout",
-                default = None,
-                help="Specify annalysis execution time limit"
-                )
-parser.add_option("-u", "--url",
-                action = "store_true",
-                dest = "url",
-                default = False,
-                help = "Specify if the target is an URL"
-                )
+def parse_args():
+    parser = argparse.ArgumentParser(description='RNAseq analysis pipeline')
+    parser.add_argument('--samples', '-s',type=str, help='RNAseq sample list')
+    parser.add_argument('--config', '-c',type=str, help='Configure path for analysys')
+    parser.add_argument('--output', '-o',type=str, help='output directory')
+    return parser.parse_args()
 
-(options,args) = parser.parse_args()
-
-def Sample(Sample_Path):
-    Samples = {}
-    with open(Sample_Path, 'r') as f:
+def sample_parse(sample_path):
+    """
+    Sample fastq file parse
+    :param Sample_Path: the file contain sample's fastq file absolute path
+    :return: dict :{'sample name':['fastq1','fastq2']}
+    """
+    samples = dict()
+    with open(sample_path, 'r') as f:
         for line in f.readlines():
             line = line.strip()
             if not len(line) or line.startswith('#'):
                 continue
             else:
-                SS = line.split('\t')
-                Samples[SS[0]] = [SS[1], SS[2]]
-    return Samples
+                ss = line.split('\t')
+                samples[ss[0]] = [ss[1], ss[2]]
 
-def Configure(Configure_Path):
-    import re
-    Conf_Dict = {}
-    Conf = open(Configure_Path,'r')
-    for line in Conf.readlines():
-        line = line.strip()
-        if not len(line) or line.startswith('#'):
-            continue
-        else:
-            pattern = re.compile(r'\S+')
-            Array = line.split('=')
-            match1 = pattern.search(Array[0])
-            match2 = pattern.search(Array[1])
-            if match1 and match2 :
-                Conf_Dict[match1.group(0)] = match2.group(0)
-    return Conf_Dict
+    return samples
 
-def Check_dir(path):
-    import os
+
+def get_options(text):
+    matches=re.findall(r'\"(.+?)\"',text)
+    print("matches:",matches)
+    return ','.join(matches)
+
+def get_option_key(text):
+    text = text.replace(' ','') # remove space
+    return text
+
+def configure(configure_path):
+    """
+    Configure file for RNAseq analysis
+    :param Configure_Path:
+    :return: dict config
+    """
+    with open(configure_path,'r') as f:
+        conf_dict = json.load(f)
+
+    return conf_dict
+
+
+def check_dir(path):
+    """
+    Create analysis directory
+    :param path: The path for directort
+    :return:
+    """
     if not os.path.exists(path):
         os.makedirs(path)
         return path
     else:
-        print path,"exists or file_name unusual!"
-
-
-# def Make_Dir(pwd):
-#
-#     ## layer1 directory
-#     Check_dir(pwd+"/"+"result")
-#     Check_dir(pwd+"/"+"shell")
-#
-#     ## layer 2 directory
-#     Check_dir(pwd + "/result/Filter")
-#     Check_dir(pwd + "/result/GenomeMapping_HISAT")
-#     Check_dir(pwd + "/result/GeneMapping_Bowtie")
-#     Check_dir(pwd + "/result/GeneExp_RSEM")
-#
-#
-#     Check_dir(pwd + "/shell/Filter")
-#     Check_dir(pwd + "/shell/GenomeMapping_HISAT")
-#     Check_dir(pwd + "/shell/GeneMapping_Bowtie")
-#     Check_dir(pwd + "/shell/GeneExp_RSEM")
+        print (path,"the path exists !")
+        return path
 
 
 
 
-##step1 filter
-def Filter_SOAPnuke(Samples,Conf_Dict):
-    Samples_Dict = {}
-    Filter_Shell_Dir = pwd + "/shell/Filter_SOAPnuke"
-    Filter_Result_Dir = pwd + "/result/Filter_SOAPnuke"
-
-    for i in Samples.keys():
-        Singel_Filter_Shell_dir = Check_dir(Filter_Shell_Dir+"/"+i)
-        Singel_Filter_Result_dir = Check_dir(Filter_Result_Dir+"/"+i)
-
-        Shell_File = Singel_Filter_Shell_dir+"/work.sh"
-        with open(Shell_File,'w') as shell:
-            shell.writelines("zcat %s %s |gzip > %s/%s.fq.gz\n"%(Samples[i][0],Samples[i][1],Singel_Filter_Result_dir,i))
-            shell.writelines("%s %s %s/%s.fq.gz\n" % (Conf_Dict['SOAPnuke'],Conf_Dict['SOAPnuke_Options'],Singel_Filter_Result_dir,i))
-        Samples_Dict[i] = "%s/%s.fq.gz" % (Singel_Filter_Result_dir,i)
-    return Samples_Dict
 
 
-##step2 alignment
-# hisat alignment of genome reference
-def GenomeMapping_HISAT(Conf_Dict,Samples_Dict):
-    HISAT = Conf_Dict['HISAT']
-    HISAT_options_PE = Conf_Dict['HISAT_options_PE']
-    HISAT_Index = Conf_Dict['HISAT_Index']
+## step1 filter
+def filter_cutadapt(samples, conf_dict, result_dir):
+    samples_dict = dict()
+    filter_shell_dir = os.path.join(result_dir, "shell/filter_cutadapt")
+    filter_result_dir = os.path.join(result_dir, "result/filter_cutadapt")
+    cutadapt = conf_dict['cutadapt']
+    for i in samples.keys():
+        singel_filter_shell_dir = check_dir(os.path.join(filter_shell_dir,i))
+        singel_filter_result_dir = check_dir(os.path.join(filter_result_dir,i))
 
-    HISAT_Shell_Dir = pwd + "/shell/HISAT"
-    HISAT_Result_Dir = pwd + "/result/HISAT"
-
-    for i in Samples_Dict.keys():
-        Singel_HISAT_Shell_dir = Check_dir(HISAT_Shell_Dir + "/" + i)
-        Singel_HISAT_Result_dir = Check_dir(HISAT_Result_Dir + "/" + i)
-
-        Shell_File = Singel_HISAT_Shell_dir + "/work.sh"
-        with open(Shell_File, 'w') as shell:
-            shell.writelines("%s %s -x %s -U %s -S %s.sam 2>%s.stat\n"%(HISAT,HISAT_options_PE,HISAT_Index,Samples_Dict[i],Singel_HISAT_Result_dir,Singel_HISAT_Result_dir))
-            shell.writelines("%s view %s.sam -b -S -o %s.bam\n"%(Conf_Dict['samtools'],Singel_HISAT_Result_dir,Singel_HISAT_Result_dir))
-
+        shell_file = os.path.join(filter_shell_dir, i, "work.sh")
+        with open(shell_file,'w') as shell:
+            shell.writelines("zcat %s %s |gzip > %s/%s.fq.gz\n"% \
+                             (samples[i][0],samples[i][1],singel_filter_shell_dir,i))
+            shell.writelines("%s %s %s/%s.fq.gz\n" % \
+                             (cutadapt['path'],cutadapt['options'],\
+                              singel_filter_result_dir,i))
+        samples_dict[i] = "%s/%s.fq.gz" % (singel_filter_result_dir,i)
+        
+    return samples_dict
 
 
-# Bowtie alignment of gene reference (for gene expression analysis)
-def GeneMapping_Bowtie(Conf_dict,Samples_Dict):
-    Bam_Dict = {}
-    Bowtie = Conf_Dict['Bowtie']
-    GeneBowtie2Index = Conf_Dict['Bowtie2Index']
-    GeneBowtie2_Options_PE = Conf_Dict['GeneBowtie2_Options_PE']
+## step2 alignment
+##  hisat alignment of genome reference
+def genomeMapping_hisat(conf_dict, samples_dict, result_dir):
+    hisat = conf_dict['hisat']
+    hisat_path = hisat['path']
+    hisat_options_pe = hisat['hisat_options_pe']
+    hisat_index = hisat['hisat_index']
 
-    Bowtie_Shell_Dir = pwd + "/shell/GeneMapping_Bowtie"
-    Bowtie_Result_Dir = pwd + "/result/GeneMapping_Bowtie"
-    for i in Samples_Dict.keys():
-        Singel_Bowtie_Shell_Dir = Check_dir(Bowtie_Shell_Dir + "/" + i)
-        Singel_Bowtie_Result_Dir = Check_dir(Bowtie_Result_Dir + "/" + i)
-        Shell_File = Singel_Bowtie_Shell_Dir + "/work.sh"
-        with open(Shell_File,'w') as shell:
-            shell.writelines("%s %s -x %s -U %s > %s/%s.sam\n"%(Bowtie,GeneBowtie2_Options_PE,Conf_Dict['Bowtie2Index'],Samples_Dict[i],Singel_Bowtie_Result_Dir,i))
-            shell.writelines("%s view %s/%s.sam -S -b -o %s/%s.bam\n"%(Conf_Dict['samtools'],Singel_Bowtie_Result_Dir,i,Singel_Bowtie_Result_Dir,i))
-        Bam_Dict[i] = Singel_Bowtie_Result_Dir+"/"+i+".bam"
-    return Bam_Dict
+    hisat_shell_dir = os.path.join(result_dir, "shell/genome_mapping_hisat")
+    hisat_result_dir = os.path.join(result_dir, "result/genome_mapping_hisat")
 
-##step3 Gene and isoform expresssion level
+    for i in samples_dict.keys():
+        singel_hisat_shell_dir = check_dir(os.path.join(hisat_shell_dir, i))
+        singel_hisat_result_dir = check_dir(os.path.join(hisat_result_dir, i))
+
+        shell_file = os.path.join(singel_hisat_shell_dir, "work.sh")
+        with open(shell_file, 'w') as shell:
+            shell.writelines("%s %s -x %s -U %s -S %s.sam 2>%s.stat\n" % \
+                             (hisat_path,hisat_options_pe,hisat_index,samples_dict[i],\
+                              singel_hisat_result_dir,singel_hisat_result_dir))
+            shell.writelines("%s view %s.sam -b -S -o %s.bam\n" % \
+                             (conf_dict['samtools'],singel_hisat_result_dir,singel_hisat_result_dir))
+
+
+
+# bowtie alignment of gene reference (for gene expression analysis)
+def gene_mapping_bowtie(conf_dict, samples_dict, result_dir):
+    bam_dict = {}
+    bowtie = conf_dict['bowtie']
+    bowtie_path = bowtie['path']
+    genebowtie2index = bowtie['bowtie2index']
+    genebowtie2_options_pe = bowtie['genebowtie2_options_pe']
+
+    bowtie_shell_dir = os.path.join(result_dir, "shell/gene_mapping_bowtie")
+    bowtie_result_dir = os.path.join(result_dir, "result/gene_mapping_bowtie")
+    for i in samples_dict.keys():
+        singel_bowtie_shell_dir = check_dir(os.path.join(bowtie_shell_dir, i))
+        singel_bowtie_result_dir = check_dir(os.path.join(bowtie_result_dir, i))
+        shell_file = os.path.join(singel_bowtie_shell_dir, "work.sh")
+        with open(shell_file,'w') as shell:
+            shell.writelines("%s %s -x %s -U %s > %s/%s.sam\n" % \
+                             (bowtie_path, genebowtie2_options_pe, \
+                              genebowtie2index, samples_dict[i],\
+                              singel_bowtie_result_dir,i))
+            shell.writelines("%s view %s/%s.sam -S -b -o %s/%s.bam\n" % \
+                             (conf_dict['samtools']['path'],singel_bowtie_result_dir,i,\
+                              singel_bowtie_result_dir,i))
+        bam_dict[i] = os.path.join(singel_bowtie_result_dir,i,".bam")
+
+    return bam_dict
+
+## step3 Gene and isoform expresssion level
 ## based on gene expression
-def GeneExp_RSEM(Conf_Dict,Samples_Dict,Bam_Dict):
+def gene_exp_rsem(conf_dict,samples_dict, bam_dict, result_dir):
     ## the bam file produced by bowtie2
-    RSEM = Conf_Dict['RSEM']
-    RSEM_Shell_Dir = pwd + "/shell/GeneExp_RSEM"
-    RSEM_Result_Dir = pwd + "/result/RSEM_Result_Dir"
-    for i in Samples_Dict.keys():
-        Singel_RSEM_Shell_Dir = Check_dir(RSEM_Shell_Dir+"/"+i)
-        Singel_RSEM_Result_Dir = Check_dir(RSEM_Result_Dir + "/" + i)
-        Shell_File = Singel_RSEM_Shell_Dir+"/work.sh"
-        with open (Shell_File,'w') as shell:
-            shell.writelines("%s --bam %s -p 8 %s %s\n"%(RSEM,Bam_Dict[i],Conf_Dict['Gene'],Singel_RSEM_Result_Dir))
+    rsem = conf_dict['rsem']['path']
+    refMrna = conf_dict['refMrna']
+    rsem_shell_dir = os.path.join(result_dir, "shell/gene_exp_rsem")
+    rsem_result_dir = os.path.join(result_dir, "result/rsem_result_dir")
+    for i in samples_dict.keys():
+        singel_rsem_shell_dir = check_dir(os.path.join(rsem_shell_dir, i))
+        singel_rsem_result_dir = check_dir(os.path.join(rsem_result_dir, i))
+        shell_file = os.path.join(singel_rsem_shell_dir, "work.sh")
+        with open (shell_file,'w') as shell:
+            shell.writelines("{bin}/rsem-calculate-expression -p 8 --bam {bam} {referMrna} {outdir}\n".format(
+                bin=rsem,bam=bam_dict[i],referMrna=refMrna,outdir=singel_rsem_result_dir))
             # 统计基因表达量
-            shell.writelines("")
+            awk_cmd = "awk '{OFS=\"\\t\";if($7!=0.00)print $1,$2,$3,$5,$7}'"
 
+            shell.writelines("{awk_cmd} {outdir}/{sample}.genes.results | grep -v '^ERCC' > {outdir}/{sample}.gene.fpkm.xls\n ".format(
+                outdir=singel_rsem_result_dir,awk_cmd=awk_cmd,sample=i)
+            )
+            shell.writelines("{awk_cmd} {outdir}/{sample}.isoforms.results | grep -v '^ERCC' > {outdir}/{sample}.transcript.fpkm.xls\n ".format(
+                outdir=singel_rsem_result_dir, awk_cmd=awk_cmd, sample=i)
+            )
 
 
 
 ##step4 Deep analysis of gene expression
-def DeneDiffExp(Conf_Dict):
-
+def gene_diff_exp(conf_dict, result_dir):
     # method 1 : PossionDis
-    PossionDis = Conf_Dict['PossionDis']
-    PossionDis_VS = T1&C1,T2&C2
-    PossionDis_Filter = Conf_Dict[PossionDis_Filter]
+    # PossionDis = conf_dict['PossionDis']
+    # PossionDis_VS = T1&C1,T2&C2
+    # PossionDis_Filter = conf_dict[PossionDis_Filter]
 
     # method 2 : DESeq
 
     # method 3 : DESeq2
+    diffexp = conf_dict['diffexp']
+
+    for key,value in diffexp['DESeq2'].items():
+        print(key,value)
+        assert(len(value)>=2)
+    # 将上面得到的表达量的数据转化成本地矩阵文件，适合DESeq2读取
+    # 执行DESeq2
+    # 获取DESeq2的结果
+    # 输出差异表达文件
 
     # method 4 : NOIseq
 
     # method 5 : EBsqe
-def Gene_PCA():
+def gene_pca():
     pass
-def Gene_Co_Expression_Network():
+def gene_co_expression_network():
     pass
-def Gene_Cluster():
+def gene_cluster():
     pass
 
 
 
+def main(args):
+    if args == None:
+        # parse_args()
+        sys.exit(1)
+
+    print(args)
+
+    samples_list = args.samples
+    configure_path = args.config
+    output_dir = args.output
+
+
+    samples = sample_parse(samples_list)
+
+    ## 获取配置文件
+    conf_dict = configure(configure_path)
+    print("conf_dict:",conf_dict)
+
+    ## 获取当前目录 为后续建立目录
+    result_dir = output_dir
+    ## 过滤数据
+    samples_dict = filter_cutadapt(samples, conf_dict, result_dir)
+    ## 基因组比对
+    genomeMapping_hisat(conf_dict, samples_dict, result_dir)
+    #
+    # ## 基因比对
+    bam_dict = gene_mapping_bowtie(conf_dict, samples_dict, result_dir)
+    # ## 表达量计算
+    gene_exp_rsem(conf_dict, samples_dict, bam_dict, result_dir)
+
+    # diff
+    gene_diff_exp(conf_dict)
 
 
 
-##step5 GO analysis
+    ##step5 GO analysis
 
-##step6 KEGG analysis
+    ##step6 KEGG analysis
 
 if __name__ == "__main__":
-    #(options, args) = parser.parse_args()
-    print '第一个位置参数',args[0]
-    print '第二个位置参数',args[1]
-    Samples_List = args[0]
-    Configure_Path = args[1]
-
-    Samples = Sample(Samples_List)
-
-## 获取配置文件
-    Conf_Dict = Configure(Configure_Path)
-
-## 获取当前目录 为后续建立目录
-    pwd = os.getcwd()
-
-## 过滤数据
-    Samples_Dict = Filter_SOAPnuke(Samples,Conf_Dict)
-## 基因组比对
-    GenomeMapping_HISAT(Conf_Dict,Samples_Dict)
-
-## 基因比对
-    Bam_Dict = GeneMapping_Bowtie(Conf_Dict,Samples_Dict)
-## 表达量计算
-    GeneExp_RSEM(Conf_Dict,Samples_Dict,Bam_Dict)
+    args = parse_args()
+    main(args)
